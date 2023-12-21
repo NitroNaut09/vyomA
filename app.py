8880f7ade9abf6b64e69707ba3ebf7338990e737
Deeraj <hrishikeshdeeraj@gmail.com>
2023-12-21 19:18:21 +0530

    Added *many* fool-proof, security related and cleaner, more robust code.

--- app.py
+++ app.py
@@ -1,10 +1,20 @@
-from flask import Flask, render_template, url_for
+import logging
+from flask import Flask, render_template, request, make_response, escape
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='templates', static_folder='static')
+logging.basicConfig(level=logging.DEBUG)
 
 @app.route('/', methods=['GET'])
 def index():
-    return render_template('index.html')
+    try:
+        user_input = request.args.get('input')
+        sanitized_input = escape(user_input)
+        response = make_response(render_template('index.html', input=sanitized_input))
+        response.headers['Content-Security-Policy'] = "default-src 'self'"
+        return response
+    except Exception as e:
+        logging.error(f"Error: {str(e)}")
+        return f"Error: {str(e)}", 500
 
 @app.route('/sw.js', methods=['GET'])
 def sw():
